3b224e3d88dfcb5bb02682cf05dfefbc59c283fb
Ron Grimes <ron.grimes@pobox.com>
2024-07-31 12:24:35 -0400

    Bunch of small refinements; debugging set to trap occasional reported but not actual update.

--- dyndns.sh
+++ dyndns.sh
@@ -23,7 +23,7 @@
 #     Note: The dyndns token may need replacing from time to time. 
 #
 # Job jar:
-#   * (none)
+#   * handle long network downtime without filling up disk with error msgs [testing]
 #
 # Updates completed (from job jar):
 #   * move variables (username, token, hostname) to shell variables for ease of updating
@@ -31,15 +31,20 @@
 #   * add note to say this script replaces dynsite
 #   * evaluate consequences of ip_file.txt not present
 #   * trap & abandon if router or internet is not running
+#   * change lasttouch file with a "touch" cmd
+#   * place msgs in strings to update editing & and allow comparisons with log file.
 #
 # Ron G
-# May 2024
+# May, June 2024
 
 ip_file=~/dyndns/ipfile.txt              # holds last ip address found
 logfile=~/dyndns/dyndns-log.txt          # log of updates
 lasttouch=~/dyndns/dyndns-lasttouch.txt  # last update - shows a heartbeat of the program
 credentials=~/dyndns/credentials.cfg     # my username, token, hostname to get IP assigned
 date_format='%F %R'
+
+msg_nochange="No change"
+msg_timeout="Timeout to get IP"
 
 source $credentials                   # Initialize the folllowing fields
 #username=myusername                  # Credentials are stored externally
@@ -56,12 +61,26 @@ else
 	stored_ip=1.2.3.4                # enable to force a change for testing purposes
 fi
 
+if [ -e $lasttouch ]
+then
+	status=`cat $lasttouch`
+else
+	status="nothing"
+fi
+
 current_ip=`curl --connect-timeout 2 ifconfig.me/ip 2> /dev/null`   # flush off stuff in stderr
 result=$?
 #echo curl = $result
 
 if [ $result = 28 ]   # Operation timeout. The specified time-out period was reached
 then	              # according to the curl conditions.
+	if [ $status = $msg_timeout ]
+	then
+		touch $lasttouch
+		exit 1
+	else
+		echo $msg_timeout > $lasttouch
+	fi
 	echo `date +"$date_format"`: Timeout to get IP address \(network down?\) | tee -a $logfile
 	echo
 	exit 1
@@ -70,21 +89,35 @@ then
 	echo `date +"$date_format"`: Unknown error to get IP address \(curl = $result\) | tee -a $logfile
 	echo
 	exit 1
+elif [ ${result:0:8} = "upstream" ]
+then	
+	echo `date +"$date_format"`: $result | tee -a $logfile
+	echo
+	exit 1
 fi
 
 #echo NAT: $current_ip
 
+
 if [ "$stored_ip" = "$current_ip" ]
 then
 	# Leave an "I was here" note & exit
-	echo `date +"$date_format"`: No change | tee $lasttouch
+	if [ -e $lasttouch ] && [ "$status" = "$msg_nochange" ]
+	then
+		touch $lasttouch
+	else
+		echo $msg_nochange | tee $lasttouch
+	fi
 	exit 0
 fi
+
+# log "$stored_ip", "$current_ip" since they're sometimes giving spurious results
+echo \"$stored_ip\", \"$current_ip\" | tee -a $logfile
 
 # save the new ip
 echo $current_ip > $ip_file
 
 # update our IP address at easydns & log the results
 curl=`curl "$dyn_update" 2> /dev/null`
-result=`echo $curl | sed -e "s/<[^>]*>//g"`    # clean off the html tags
-echo `date +"$date_format"`: $result | tee -a $logfile
+update=`echo $curl | sed -e "s/<[^>]*>//g"`    # clean off the html tags
+echo `date +"$date_format"`: $update | tee -a $logfile
